77bce3dd87cfd284d54e0a8849950b0959844065
Sabrina Chowdhury <nebula155@gmail.com>
2019-05-02 16:13:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,11 +16,11 @@ class Lunch {
 }
 
 class Dinner {
-  constructor(salad, soup, entree, _dessert) {
+  constructor(salad, soup, entree, dessert) {
     this.salad = salad;
     this.soup = soup; 
     this.entree = entree; 
-    this.dessert = dessert;
+    this._dessert = dessert;
   }
 }
 
